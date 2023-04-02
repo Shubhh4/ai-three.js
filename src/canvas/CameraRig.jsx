2f5d005eab5f636c5c29c3fee9448e88778f5dc2
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2023-04-02 12:39:15 +0530

    Update CameraRig.jsx

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -13,7 +13,7 @@ const CameraRig = ({ children }) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
-    // set the initial position of the model
+    //set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
     if(snap.intro) {
       if(isBreakpoint) targetPosition = [0, 0, 2];
